86e5a6dd406970461f4dce3eb6da3eef72e2101e
Duc <duc@megapis.com>
2020-12-21 00:11:19 +0700

    style using react-bootstrap

--- reactjs-client/src/App.js
+++ reactjs-client/src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
+import { Form, Col, Button } from 'react-bootstrap';
 
 function App() {
   const [number, setNumber] = useState(null);
@@ -21,15 +23,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Find highest prime number</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Number:
-          <input type="number" value={number} onChange={(e) => setNumber(e.target.value)}/>
-          Result: {result}
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+        <h1 className="mb-4">Find highest prime number</h1>
+        <Form onSubmit={handleSubmit}>
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridNumber">
+              <Form.Label>Number</Form.Label>
+              <Form.Control type="number" placeholder="Enter number" value={number}
+              onChange={(e) => setNumber(e.target.value)}/>
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridResult">
+              <Form.Label>Highest Prime</Form.Label>
+              <Form.Control type="text" readOnly value={result}/>
+            </Form.Group>
+          </Form.Row>
+          <Button size="lg" variant="primary" type="submit" block>
+            Submit
+          </Button>
+        </Form>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
